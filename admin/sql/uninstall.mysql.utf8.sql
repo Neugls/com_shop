8689401e9a681bce454561beb490ba4ae3893420
Alonzo Turner <alonzo.turner@subtextproductions.com>
2014-11-12 18:18:06 -0600

    FIXED: Add drop statement to uninstall for item_options

--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS `#__shop_carts`;
 DROP TABLE IF EXISTS `#__shop_cart_items`;
 DROP TABLE IF EXISTS `#__shop_customers`;
 DROP TABLE IF EXISTS `#__shop_images`;
+DROP TABLE IF EXISTS `#__shop_item_options`;
 DROP TABLE IF EXISTS `#__shop_options`;
 DROP TABLE IF EXISTS `#__shop_products`;
